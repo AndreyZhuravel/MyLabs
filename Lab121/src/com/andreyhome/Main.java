1a736d3c172641d81ac77dba3afd409f80460ebf
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-09-07 21:55:35 +0300

    Lab121,Lab122

---
+++ Lab121/src/com/andreyhome/Main.java
@@ -0,0 +1,12 @@
+package com.andreyhome;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the name of the student");
+        System.out.println("Enter the name of the student");
+        System.out.println("Enter the name of the student");
+        System.out.println("Enter the name of the student");
+        System.out.println("Enter the name of the student");
+    }
+}
---
+++ Lab122/src/com/andreyhome/Main.java
@@ -0,0 +1,13 @@
+package com.andreyhome;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of the student");
+        String test = sc.nextLine();
+        System.out.println("Name of the student is..." + test +" :_) !");
+    }
+}
