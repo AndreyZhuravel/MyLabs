760208930136e4c2463a8fa231a0ad2e4cc86725
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-09-09 12:20:36 +0300

    This is Lab122

--- Lab122/src/com/andreyhome/Main.java
+++ Lab122/src/com/andreyhome/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner me = new Scanner(System.in); //создаем новый объект сканнер
+        Scanner me = new Scanner(System.in); //создаем новый  объект сканнер
         System.out.println("Enter the name of the student"); // выводим строку с приветствием
         String test = me.nextLine(); //считываем введенную информацию через строку
 
