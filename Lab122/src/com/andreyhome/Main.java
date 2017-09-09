0a312b6a2f50bf678de6690d80c371a7fc1e5522
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-09-09 12:30:09 +0300

    This is my comments to Lab122

--- Lab122/src/com/andreyhome/Main.java
+++ Lab122/src/com/andreyhome/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner me = new Scanner(System.in); //создаем  новый  объект  сканнер
         System.out.println("Enter the name of the student"); // выводим строку с приветствием
-        String test = me.nextLine(); //считываем введенную информацию через строку
+        String test = me.nextLine(); //считываем введенную информацию с использованием строки
 
         System.out.println("Name of the student is..." + test +" :_) !"); //выводим строку пользователю
     }
