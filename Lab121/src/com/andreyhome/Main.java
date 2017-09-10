ebcfd2e50ebf0725a67ad87145d442d8c22f04d3
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-09-10 15:13:27 +0300

    Simple commit

--- Lab121/src/com/andreyhome/Main.java
+++ Lab121/src/com/andreyhome/Main.java
@@ -3,7 +3,7 @@ package com.andreyhome;
 public class Main {
 
     public static void main(String[] args) {
-        //тренируемся ставить звездочки
+        //тренируемся ставить звездочки и всякие штучки
          System.out.println( "   * * *    *   *       *    *");
          System.out.println( "       *  *   *  *     *   *   *");
          System.out.println( "       * *     *  *   *   *     *");
