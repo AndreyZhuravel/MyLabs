d64003583d67ddbb5c8ffba556abd8c9876b2a0f
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-10-08 18:51:34 +0300

    labs 24* incoming

--- Lab246/src/com/andreyhome/Main.java
+++ Lab246/src/com/andreyhome/Main.java
@@ -7,9 +7,44 @@ of numbers of height h. Example: For h = 4 the method should print the pyramid 1
 
  */
 
+class MyPyramid {
+
+    static void printPyramid(int h) {
+
+        int countrow = 1;
+
+        for (int i = h; i > 0; i--) //Организовываем цикл по высоте = количеству строк
+        {
+            for (int j = 1; j <= i*2; j++)
+            {
+                System.out.print(" "); //Печатаем 2 пробела между цифрами
+            }
+
+            //Счет цифр в порядке возрастания
+            for (int j = 1; j <= countrow; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            //Счет цифр в порядке убывания
+            for (int j = countrow-1; j >= 1; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+
+            countrow++;
+        }
+
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        MyPyramid piramidka = new MyPyramid();
+        piramidka.printPyramid(4);
     }
 }
